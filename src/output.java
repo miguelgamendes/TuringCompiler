fc9888a5b9302af238e1079b1b925c925bfe7a62
zephiro309 <zephiro309@gmail.com>
2015-06-12 01:34:40 +0100

    Fixed several code generation errors, added debug flag for compilation and execution.

--- src/output.java
+++ src/output.java
@@ -24,6 +24,18 @@ public class output {
 			}
 		}
 
+	boolean debug = false;
+
+	if(args.length > 2) {
+		System.out.println("TURING MACHINE USAGE: java -jar YourTuringMachine.jar inputString [-d]");
+		System.exit(1);
+	} else {
+		if(args.length == 2 && args[1].equals("-d")) {
+			debug = true;
+		}
+	}
+
+
 	boolean finished = false; //for when the line specifies the analysis is over
 	int currentState = 0;
 	int currentIndex = 0;
@@ -32,30 +44,49 @@ public class output {
 
 	//variable section of code is here
 		cards.addElement(new Card(0));
-		cards.elementAt(0).addLine('a', 'b', 'r', 0);
-		cards.elementAt(0).addLine('c', 'd', 'l', 1);
+		cards.elementAt(0).addLine('a', 'a', 'r', 1);
+		cards.elementAt(0).addLine('b', 'b', 'r', 0);
+		cards.elementAt(0).addLine('c', 'c', 'r', 0);
 		cards.addElement(new Card(1));
-		cards.elementAt(1).addLine('b', 'b', 'l', 1);
-		cards.elementAt(1).addLine('c', 'd', 'r', -1);
+		cards.elementAt(1).addLine('a', 'a', 'r', 1);
+		cards.elementAt(1).addLine('b', 'b', 'r', 2);
+		cards.elementAt(1).addLine('c', 'c', 'r', 0);
 		cards.addElement(new Card(2));
-		cards.elementAt(2).addLine('b', 'b', 'r', 2);
+		cards.elementAt(2).addLine('a', 'a', 'r', 1);
+		cards.elementAt(2).addLine('b', 'b', 'r', 0);
+		cards.elementAt(2).addLine('c', 'd', 'l', 3);
 		cards.addElement(new Card(3));
+		cards.elementAt(3).addLine('a', 'd', 'r', -1);
+		cards.elementAt(3).addLine('b', 'd', 'l', 3);
+		cards.elementAt(3).addLine('c', 'd', 'l', 3);
 	//end of variable section
 	StringBuilder input = new StringBuilder(args[0]);
 
 	while(!finished) {
+		if(debug) {
+			System.out.println(input);
+			for(int i = 0; i < input.length(); i++) {
+				if(i == currentIndex)
+					System.out.print("^");
+				else
+					System.out.print(" ");
+				if(i == input.length() - 1)
+					System.out.println("  " + currentState);
+			}
+		}
+
 		//get char
 		currentChar = input.charAt(currentIndex);
 		//process char
-		input.setCharAt(currentIndex, cards.elementAt(currentIndex).readWrite.get(currentChar).charValue());
+		input.setCharAt(currentIndex, cards.elementAt(currentState).readWrite.get(currentChar).charValue());
 		//jump or end
-		if(cards.elementAt(currentIndex).readJump.get(currentChar).intValue() == -1) {
+		if(cards.elementAt(currentState).readJump.get(currentChar).intValue() == -1) {
 			finished = true;
 			continue;
 		} else
-			currentState = cards.elementAt(currentIndex).readJump.get(currentChar).intValue();
+			currentState = cards.elementAt(currentState).readJump.get(currentChar).intValue();
 		//move
-		if(cards.elementAt(currentIndex).readMove.get(currentChar).charValue() == 'r')
+		if(cards.elementAt(currentState).readMove.get(currentChar).charValue() == 'r')
 			currentIndex++;
 		else
 			currentIndex--;
